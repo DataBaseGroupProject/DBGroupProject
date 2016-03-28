af0648c3c08c1fd7b6cefa05cbe3a0605afda4e3
Humam Al-Faiz <humam.alfaiz@gmail.com>
2016-03-28 19:45:58 -0400

    Test

---
+++ DBGroupProject/DataAccess.cs
@@ -0,0 +1,28 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+
+namespace DBGroupProject
+{
+    class DataAccess
+    {
+        public static void CreateOracleConnection()
+        {
+        }
+
+        public static OracleConnection con;
+
+        public static void Connect()
+        {
+            con = new OracleConnection();
+            con.ConnectionString = "Data Source=XE;User Id=system;Password=admin;";
+            con.Open();
+            Console.WriteLine("Connected to Oracle" + con.ServerVersion);
+        }
+
+        public static void Close()
+        {
+            con.Close();
+            con.Dispose();
+        }
+    }
+}
--- DBGroupProject/Form1.Designer.cs
+++ DBGroupProject/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace DBGroupProject
+﻿using System.Configuration;
+
+
+namespace DBGroupProject
 {
     partial class Form1
     {
@@ -31,6 +34,8 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Text = "Form1";
+
+            DataAccess.Connect();
         }
 
         #endregion
